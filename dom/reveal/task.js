9749c7c946e2f91fe4c2e13cc3380d181a4f7cb5
arrowls <babuyoff@gmail.com>
2022-07-17 21:26:22 +0300

    added improvements to reveal task

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,8 +3,11 @@ const revealElements = document.querySelectorAll('.reveal');
 function revealOnScroll() {
     revealElements.forEach((element) => {
         const elementPosition = element.getBoundingClientRect();
-        if (elementPosition.top > 0) {
+        if (elementPosition.top < window.innerHeight && elementPosition.bottom < window.innerHeight) {
             element.classList.add('reveal_active');
+        }
+        if (elementPosition.top < 0 || elementPosition.bottom > (window.innerHeight + elementPosition.height)) {
+            element.classList.remove('reveal_active');
         }
     });
 }
